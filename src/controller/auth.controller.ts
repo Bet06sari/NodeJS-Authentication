c079927b89729a798e6a5c06d30ac138370f27ae
Betül Sarı <52050427+Bet06sari@users.noreply.github.com>
2024-02-19 17:30:08 +0300

    login step completed

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -25,3 +25,34 @@ export const Register = async (req: Request, res: Response) => {
         res.status(500).json({ error: "Sunucu hatası" });
       }
 };
+
+export const Login = async  (req: Request, res: Response) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+      return res.status(400).json({ 
+        message: "Kullanıcı bulunamadı" 
+      });
+    }
+
+    if (!user.password) {
+      return res.status(400).json({ 
+        message: "Kullanıcı şifresi bulunamadı" 
+      });
+    }
+
+    const isPasswordValid = await bcryptjs.compare(req.body.password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({
+        message: "Şifre hatalı"
+      });
+    }
+
+    res.send(user);
+
+  } catch (error) {
+    console.error("Kullanıcı girişi yapılırken hata:", error);
+    res.status(500).json({ error: "Sunucu hatası" });
+  }
+};
