732f437436a3ccb5bda4feb38354c4fe1cb4cf5a
Betül Sarı <52050427+Bet06sari@users.noreply.github.com>
2024-02-19 15:40:59 +0300

    Implement database operations using TypeORM

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,21 @@
 import dotenv from 'dotenv';
 import express, { Request, Response } from "express";
 import mongoose from 'mongoose';
+import { createConnection } from 'typeorm';
 
 dotenv.config();
+
 const app = express();
 app.use(express.json());
 
-const MONGODB_URI: string | undefined = process.env.MONGODB_URI;
-
-mongoose.connect(MONGODB_URI!, {
-}).then(() => {
-  console.log("MongoDB'ye bağlantı başarılı");
-}).catch((err) => {
-  console.error("MongoDB'ye bağlanırken hata oluştu:", err);
-  process.exit(1);
+createConnection().then(() => {
+  console.log("database connection successful");
+  app.get("/", (req: Request, res: Response) => {
+    res.send("Hello World!");
+  });
+  
+  app.listen(3000, () => {
+    console.log("Example app listening on port 3000!");
+  });
 });
 
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
-
-app.listen(3000, () => {
-  console.log("Example app listening on port 3000!");
-});
